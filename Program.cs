05ca724fee309d98c9f73dd0b64b8aef0c6b63d9
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-21 21:34:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MEDIDAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  entrada ;
+            MEDIDAS centimetros , quilometros, metros ;
+
+           
+            Console.WriteLine ( " Digite a medida em metros: " );
+            entrada  =  Console.ReadLine ();
+            medidas  =  Convert.ToDouble ( entrada );
+            centimetros  =  metros  *  100 ;
+            quilometros  =  metros  /  100 ;
+            
+            Console. ForegroundColor = ConsoleColor.Cyan; 
+            Console.WriteLine ( " \n Equivalência" );
+            Console.WriteLine ($" { centimetros } cm " );
+            Console.WriteLine ($" { metros } m " );
+            Console.WriteLine ($" { quilometros  } km " );
+            ;
+    }
+}
